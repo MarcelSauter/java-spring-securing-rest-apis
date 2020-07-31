bb55fe4ad2b2c394827fcb1a26291787d601aa8a
Marcel Sauter <msauter@proqrent.de>
2020-07-31 10:10:56 +0200

    "Completed Module"

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionInitializer.java
@@ -8,9 +8,11 @@ import java.util.UUID;
 @Component
 public class ResolutionInitializer implements SmartInitializingSingleton {
 	private final ResolutionRepository resolutions;
+	private final UserRepository userRepository;
 
-	public ResolutionInitializer(ResolutionRepository resolutions) {
+	public ResolutionInitializer(ResolutionRepository resolutions, UserRepository userRepository) {
 		this.resolutions = resolutions;
+		this.userRepository = userRepository;
 	}
 
 	@Override
@@ -18,5 +20,23 @@ public class ResolutionInitializer implements SmartInitializingSingleton {
 		this.resolutions.save(new Resolution("Read War and Peace", "user"));
 		this.resolutions.save(new Resolution("Free Solo the Eiffel Tower", "user"));
 		this.resolutions.save(new Resolution("Hang Christmas Lights", "user"));
+
+		User user = new User("user",
+				"{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		user.grantAuthority("resolution:read");
+		user.grantAuthority("resolution:write");
+		this.userRepository.save(user);
+
+		User hasRead = new User();
+		hasRead.setUsername("hasRead");
+		hasRead.setPassword("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		hasRead.grantAuthority("resolution:read");
+		this.userRepository.save(hasRead);
+
+		User hasWrite = new User();
+		hasWrite.setUsername("hasWrite");
+		hasWrite.setPassword("{bcrypt}$2a$10$MywQEqdZFNIYnx.Ro/VQ0ulanQAl34B5xVjK2I/SDZNVGS5tHQ08W");
+		hasWrite.grantAuthority("resolution:write");
+		this.userRepository.save(hasWrite);
 	}
 }
